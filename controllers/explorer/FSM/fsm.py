f31340bf20ef9fc7550bfdee6eb57c344e660628
Tukhvatullin Timur <timtuvad@gmail.com>
2026-05-01 23:59:07 +0300

    .

--- controllers/explorer/FSM/fsm.py
+++ controllers/explorer/FSM/fsm.py
@@ -1,4 +1,8 @@
+from map import Direction, MapPoint, Map
 
 class FSMHandler:
-    def __init__(self):
+    def __init__(self, world_map:Map):
+        self.world_map = world_map
+    
+    def tick(self, x:float, y:float, theta:float, point:MapPoint, direction:Direction):
         pass
